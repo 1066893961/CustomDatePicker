ccc9c7f602f2c60a460397dcac60f353af53d766
lwz <m18911450394@163.com>
2018-11-29 17:13:16 +0800

    dialog点击外部消失

--- app/src/main/java/com/liuwan/customdatepicker/widget/CustomDatePicker.java
+++ app/src/main/java/com/liuwan/customdatepicker/widget/CustomDatePicker.java
@@ -85,7 +85,7 @@ public class CustomDatePicker {
     private void initDialog() {
         if (datePickerDialog == null) {
             datePickerDialog = new Dialog(context, R.style.time_dialog);
-            datePickerDialog.setCancelable(false);
+            datePickerDialog.setCancelable(true);
             datePickerDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
             datePickerDialog.setContentView(R.layout.custom_date_picker);
             Window window = datePickerDialog.getWindow();
